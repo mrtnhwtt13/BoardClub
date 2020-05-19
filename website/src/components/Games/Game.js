fb5014d02f0ee1ff36a7585223b72892219b9243
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-05-19 12:10:42 +0200

    add image and boardgame details in card

--- website/src/components/Games/Game.js
+++ website/src/components/Games/Game.js
@@ -1,20 +1,67 @@
 import React, { Component } from 'react';
 import { withStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import { CardMedia } from '@material-ui/core';
 import { Link } from 'react-router-dom';
-
+import axios from 'axios';
+import Grid from '@material-ui/core/Grid';
 
 class Game extends Component {
+    constructor (props) {
+        super(props);
+
+        this.state = {
+            boardGameDetails: null,
+            loading: true
+        }
+    }
+
+    componentDidMount() {
+        axios.get('https://bgg-json.azurewebsites.net/thing/'+ this.props.game.boardGameId)
+        .then(response => this.setState({
+            boardGameDetails: response,
+            loading: false
+        })
+        )
+        .catch(err => console.log(err))
+    } 
+
     render () {
         const { classes, game } = this.props;
+        const { boardGameDetails, loading } = this.state;
+        let boardGameInfos = null;
+
+        if (loading === false) {
+            boardGameInfos = (
+                <div className={classes.gameBlock}>
+                    <img
+                        src={boardGameDetails.data.thumbnail}
+                        height="100"
+                    />
+                    <span className={classes.bgTitle}><strong>Playing : </strong>{boardGameDetails.data.name}</span>
+                </div>
+            )
+        }
 
         return (
             <Paper className={classes.paper}>                 
                 <div>
-                    <h3 className={classes.login}>
-                        {game.title}
-                        <span className={classes.time}>{(new Date(game.gameDate)).toLocaleString()}</span>
-                    </h3>
+                    <Grid
+                        container
+                        direction="row"
+                        justify="center"
+                        alignItems="center"
+                    >
+                        <h3 className={classes.title}>
+                            {game.title}
+                        </h3>
+                        <h4>
+                            {game.playersNumber}/{game.playersMax}  
+                        </h4>        
+                    </Grid>
+                    <span className={classes.time}>Event on {(new Date(game.gameDate)).toLocaleString()} at {game.city}</span>
+                    <br/>
+                    {boardGameInfos}
                 </div>
             </Paper>
         )
@@ -28,40 +75,19 @@ const styles = {
         display: 'flex',
         marginTop: 10
     },
-    avatar: {
-        minWidth: 10,
-        margin: '4px 10px 4px 4px'
-    },
-    login: {
+    title: {
         marginBottom: 5
     },
     time: {
-        marginLeft: 10,
         color: '#bbb',
         fontSize: 14
     },
-    btnBlock: {
+    gameBlock: {
         width: '100%',
-        textAlign: 'right'
+        marginTop: 10
     },
-    btnDelete: {
-        backgroundColor: '#7584ff',
-        color: 'white',
-        '&:hover': {
-            color: '#7584ff',
-            borderColor: '#7584ff',
-            backgroundColor: 'white'
-        }
-    },
-    btnEdit: {
-        marginRight: "3px",
-        backgroundColor: '#7584ff',
-        color: 'white',
-        '&:hover': {
-            color: '#7584ff',
-            borderColor: '#7584ff',
-            backgroundColor: 'white'
-        }
+    bgTitle: {
+        marginLeft: 10
     }
 }
 
