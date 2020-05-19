43adeb9548b0793d17e9d7d518b12d81315b77ae
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-05-19 18:08:09 +0200

    homepage almost ok - not responsive yet

--- website/src/components/Layout/Main.js
+++ website/src/components/Layout/Main.js
@@ -8,7 +8,7 @@ const Main = ({ children }) => (
     <div>
         <Header />
         <Grid container justify="center">
-            <Grid item xs={12} sm={6} style={{ marginTop: 30 }}>
+            <Grid item xs={12} sm={6} style={{ marginTop: 10 }}>
                 {children}
             </Grid>
         </Grid>
