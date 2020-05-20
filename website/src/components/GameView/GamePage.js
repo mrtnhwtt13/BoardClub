b02368c56a6c738400603afd99e34c66fb401fdd
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-05-20 17:41:18 +0200

    request ok

--- website/src/components/GameView/GamePage.js
+++ website/src/components/GameView/GamePage.js
@@ -16,73 +16,89 @@ class Gamepage extends Component {
     
         this.state = {
         boardGameDetails: null,
-        loading: true,
+        loadingBoardGameDetails: true,
         }
+
+        this.getBoardGameDetails = this.getBoardGameDetails.bind(this);
     }
 
     componentDidMount() {
-        this.props.getGameById(this.props.match.params.gameId);
-        /*
-        axios
-    .get(
-        'https://bgg-json.azurewebsites.net/thing/' +
-        this.props.match.params.gameId,
-    )
-    .then((response) =>{
-        this.setState({
-        boardGameDetails: response,
-        loading: false,
-        }),
-        console.log(response)}
-    )
-    .catch((err) => console.log(err))
-    */
-    }  
+        this.props.getGameById(this.props.match.params.gameId)
+    }
+
+    getBoardGameDetails (bgId) {
+        axios.get('https://bgg-json.azurewebsites.net/thing/' + bgId)
+            .then((response) =>{
+                this.setState({
+                    boardGameDetails: response,
+                    loadingBoardGameDetails: false
+                })
+            })
+            .catch((err) => console.log(err))		
+    }     
 
     render(){
-        const { classes } = this.props;
-        
-        return (
-            <div>
-            <div>
-            <Grid container spacing={2} direction="row" className={classes.bgBlock}>
-            <Grid item>    
-            Image
-            </Grid> 
-            <Grid item xs={7} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-            <Grid item xs>
-                <Typography gutterBottom variant="subtitle1">
-                <div className={classes.title}>Title</div>
-                </Typography>
-                <Typography variant="body2" gutterBottom>
-                Game
-                </Typography>
-                <Typography variant="body2" color="textSecondary">
-                <span className={classes.time}>
-                    Time
-                </span>
-                </Typography>
-            </Grid>
-            
-            </Grid>
-            <Grid item>
-            <Typography variant="subtitle1">
+        const { classes, loadingGame } = this.props;
+        const { boardGameDetails, loadingBoardGameDetails } = this.state;
+
+        if (loadingGame === false && boardGameDetails === null) {
+            this.getBoardGameDetails(this.props.list.boardGameId);
+        }
+
+        if (loadingBoardGameDetails === true) {
+            return (
                 <div>
-                    NaN/NaN
-                </div></Typography>
-            </Grid>
-            </Grid> 
-            </Grid>
-            </div>
-        {/* </Paper> */}
-            </div>
-        )
-    }
+                    loading
+                </div>
+            )
+        }
+        else {
+            return (
+                <div>
+                <div>
+                <Grid container spacing={2} direction="row" className={classes.bgBlock}>
+                <Grid item>    
+                Image
+                </Grid> 
+                <Grid item xs={7} sm container>
+                <Grid item xs container direction="column" spacing={2}>
+                <Grid item xs>
+                    <Typography gutterBottom variant="subtitle1">
+                    <div className={classes.title}>Title</div>
+                    </Typography>
+                    <Typography variant="body2" gutterBottom>
+                    Game
+                    </Typography>
+                    <Typography variant="body2" color="textSecondary">
+                    <span className={classes.time}>
+                        Time
+                    </span>
+                    </Typography>
+                </Grid>
+                
+                </Grid>
+                <Grid item>
+                <Typography variant="subtitle1">
+                    <div>
+                        NaN/NaN
+                    </div></Typography>
+                </Grid>
+                </Grid> 
+                </Grid>
+                </div>
+            {/* </Paper> */}
+                </div>
+            )
+                    
+        }
+        
+    }   
 }
 
 const mapStateToProps = (state) => ({
     isAuthenticated: state.auth.isAuthenticated,
+    list: state.game.list,
+    loadingGame: state.game.loading
   })
 
 const styles = {
