d2a25c903b0b18dec0cff1fba8d10073d9c2133b
Sébastien CHOUBRAC <sebastien.choubrac@epitech.eu>
2020-05-26 14:18:32 +0200

    creation of comments model

---
+++ server/models/Comment.js
@@ -0,0 +1,20 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Create Schema
+const commentSchema = new Schema({
+  user: {
+    type: Schema.Types.Object,
+    required: true
+  },
+  text: {
+    type: String,
+    required: true
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now
+  }
+});
+
+module.exports = mongoose.model("Comment", commentSchema);
