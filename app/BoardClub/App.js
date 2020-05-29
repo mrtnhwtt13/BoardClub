9f58d52379bd4cf5e1bc7a5d64ef7541192ee6f8
Martin Hewitt <martin.hewitt@epitech.eu>
2020-05-29 16:03:11 +0200

    base app

--- app/BoardClub/App.js
+++ app/BoardClub/App.js
@@ -1,19 +1,38 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Login from './pages/Login';
+
+
+const Stack = createStackNavigator();
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-    </View>
-  );
-}
+    return (
+        <NavigationContainer>
+            <Stack.Navigator
+                initialRouteName="Login"
+                screenOptions={{
+                    headerStyle: {
+                        backgroundColor: '#1e3f83',
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        fontWeight: 'bold',
+                    },
+                    headerTitleAlign: 'center'
+                }}
+            >
+                <Stack.Screen
+                    name="Login"
+                    options={{headerShown: false}}
+                    component={Login}
+                />
+                                    
+            </Stack.Navigator>
+        </NavigationContainer>
+    );
+} 
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+//console.disableYellowBox = true;
