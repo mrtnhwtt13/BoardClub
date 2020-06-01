5962d67ed8d7f3039e468d7b4403be9b4eb6b3c7
Martin Hewitt <martin.hewitt@epitech.eu>
2020-06-01 10:21:21 +0200

    mobile server game

---
+++ server/routes/mobile/game.js
@@ -0,0 +1,77 @@
+const router = require("express").Router();
+const Game = require("../models/Game");
+const User = require("../models/User");
+
+// get all upcoming games sorted by game date
+router.route('/upcoming')
+    .get((req, res) => {
+        Game.find({ gameDate: {$gte: new Date()}})
+            .sort({ gameDate: 1 })
+            .then(games => res.json(games))
+            .catch(err => console.log(err))
+    })
+
+
+    // get one game by game Id
+router.route('/find/:gameId')
+.get((req, res) => {
+    Game.findById(req.params.gameId)
+        .then(game => {
+            if (game) {
+                return res.json(game)
+            }
+            else {
+                return res.status(404).json({ message: 'Game not found'})
+            }
+        })
+        .catch(err => console.log(err))                
+})
+
+// join a game
+router.route('/join')
+    .post(
+        passport.authenticate('jwt', { session: false }),
+        (req, res) => {
+            Game.findOneAndUpdate({
+                _id: req.body.gameId
+            }, {
+                $push: { players: req.user.id}
+            },
+            { new: true})
+            .then(game1 => {
+                Game.findOneAndUpdate({
+                    _id: req.body.gameId
+                }, {
+                    playersNumber: (game1.playersNumber + 1)
+                }, { new: true })
+                .then(game2 => res.json({ players: game2.players }))
+                .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
+        }
+    )
+
+
+// leave a game
+router.route('/leave')
+    .post(
+        passport.authenticate('jwt', { session: false }),
+        (req, res) => {
+            Game.findOneAndUpdate({
+                _id: req.body.gameId
+            }, {
+                $pull: { players: req.user.id}
+            },
+            { new: true })
+            .then(game1 => {
+                Game.findOneAndUpdate({
+                    _id: req.body.gameId
+                }, {
+                    playersNumber: (game1.playersNumber - 1)
+                }, { new: true })
+                .then(game2 => res.json({ players: game2.players }))
+                .catch(err => console.log(err))
+            })
+            .catch(err => console.log(err))
+        }
+    )
